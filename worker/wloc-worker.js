4df466f98ae557e5a496c6b16b55a75d144f3c84
Admin <168221282+us40011@users.noreply.github.com>
2026-08-12 15:54:48 +0800

    Apple New

--- worker/wloc-worker.js
+++ worker/wloc-worker.js
@@ -262,8 +262,8 @@ body { font-family:-apple-system,system-ui,sans-serif; background:var(--bg); col
         <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path>
       </svg>
     </div>
-    <div class="footer-brand" style="font-weight:600;font-size:15px;color:#1c1c1e;margin-bottom:4px">我爱研究 ILovestudy</div>
-    <div class="footer-version" style="font-size:12px;color:var(--gray);margin-bottom:14px;font-family:monospace;font-weight:700;">v1.1.9</div>
+    <div class="footer-brand" style="font-weight:600;font-size:15px;color:#1c1c1e;margin-bottom:4px">澳门银河网络科技ILovestudy</div>
+    <div class="footer-version" style="font-size:12px;color:var(--gray);margin-bottom:14px;font-family:monospace;font-weight:700;">微信 VIP4001177</div>
     <a class="btn btn-tg" href="https://t.me/VIP111177" target="_blank">Telegram</a>
   </div>
 
